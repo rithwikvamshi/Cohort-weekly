1ceb93c4e51376f8741b8629708c11721e6188f5
rithwikvamshi <rithwikvamshi@gmail.com>
2024-01-28 15:09:06 -0600

    Solved week-3/ 01 & 02

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -1,27 +1,31 @@
 const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
-const app = express();
-// You have been given an express server which has a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// rate limit the requests from a user to only 5 request per second
-// If a user sends more than 5 requests in a single second, the server
-// should block them with a 404.
-// User will be sending in their user id in the header as 'user-id'
-// You have been given a numberOfRequestsForUser object to start off with which
-// clears every one second
 
-let numberOfRequestsForUser = {};
-setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+const app = express();
+let errorCount = 0;
+
+// You have been given an express server which has a few endpoints.
+// Your task is to
+// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
 app.get('/user', function(req, res) {
+  throw new Error("User not found");
   res.status(200).json({ name: 'john' });
 });
 
 app.post('/user', function(req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
+
+app.get('/errorCount', function(req, res) {
+  res.status(200).json({ errorCount });
+});
+
+app.use(function(err,req,res,next){
+  res.status(404).send({});
+  errorCount=errorCount+1;
+})
 
 module.exports = app;
